d9f5e3602bda15ca29a3924173d7c78bc6d55450
None <None>
2020-01-30 11:53:19 -0800

    Initial implementation of example class to control load balancing.

--- webServer.py
+++ webServer.py
@@ -2,8 +2,35 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 from http.server import HTTPServer
 
+
+class Host():
+	def __init__(self, IP):
+		self.ip = IP
+		self.connections = 0
+		self.accepting = True
+
+	def Accepting(self):
+		return self.accepting
+
+	def GetIp(self):
+		return self.ip
+
+class LoadBalancer():
+	def __init__(self):
+		#self.hosts = [ Host('10.0.0.2') ]
+		self.hosts = [ Host('https://www.google.com'),
+				Host('https://www.wikipedia.org') ]
+
+	def GetNextHost(self):
+		# Determine the next host to balance to
+		for host in self.hosts:
+			if (host.Accepting()):
+				return host.GetIp()
+		
+
 class GetHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
+		global lb
 		parsed_path = parse.urlparse(self.path)
 		message_parts = [
 			'CLIENT VALUES:',
@@ -32,12 +59,13 @@ class GetHandler(BaseHTTPRequestHandler):
 		self.send_response(301)
 		self.send_header('Content-Type', 'text/plain; charset=utf-8')
 		# This will redirect it to another host
-		self.send_header('Location', 'https://www.google.com')
+		self.send_header('Location', lb.GetNextHost())
 		self.end_headers()
 		self.wfile.write(message.encode('utf-8'))
 
 
 if __name__ == '__main__':
+	lb = LoadBalancer()
 	server = HTTPServer(('localhost', 8080), GetHandler)
 	print('Starting server, use <Ctrl-C> to stop')
 	server.serve_forever()
