1f045fc7f2aa290e4140ce514e0ce5091dffeb44
Terri Hewitt <hewittt@oregonstate.edu>
2020-02-29 14:39:52 -0800

    Add threaded client handling

--- loadBalancedServer.py
+++ loadBalancedServer.py
@@ -1,19 +1,22 @@
 import socket
-from threading import Thread, Lock
+from _thread import *
+#from threading import Thread, Lock
+import threading
 import json
 import time
 
 # Main thread keeps a socket open and appends to the list
 LOCALHOST = "127.0.0.1"
 LB_PORT = 4000
+CLIENT_PORT = 4001
 
-mutex = Lock()
+mutex = threading.Lock()
 
 clients = 0
 
-class LoadBalancerCommThread(Thread):
+class LoadBalancerCommThread(threading.Thread):
   def __init__(self):
-    Thread.__init__(self)
+    threading.Thread.__init__(self)
     self.reqSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.reqSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.reqSocket.bind((LOCALHOST, LB_PORT))
@@ -42,9 +45,45 @@ class LoadBalancerCommThread(Thread):
       finally:
         mutex.release()
 
+class ClientCommThread(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.reqSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.reqSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.reqSocket.bind((LOCALHOST, CLIENT_PORT))
+    print("Server started on %s:%d" % (LOCALHOST, CLIENT_PORT))
+
+  def threadSafePrint(self, msg):
+    mutex.acquire()
+    print(msg)
+    mutex.release()
+
+  def handleClient(self, client): # Threaded function
+    while (True):
+      data = client.recv(1024)
+      if not data:
+        self.threadSafePrint('Closing connection to client')
+        break
+      mutex.acquire()
+      client.send(data) # Echo data back to client
+      mutex.release()
+    client.close()
+
+  def run(self):
+    self.reqSocket.listen(1)
+
+    allowedThreadCount = threading.active_count() + 5
+    while (True):
+      if (threading.active_count() < allowedThreadCount):    # Allow up to 5 running threads at a time
+        self.clientSock, self.clientAddr = self.reqSocket.accept()
+        self.threadSafePrint('Connected to :' + str(self.clientAddr[0]) + ':' + str(self.clientAddr[1]))
+        thread = threading.Thread(target=self.handleClient, args=(self.clientSock,))
+        thread.start()
+        self.threadSafePrint("Number of threads running is now: "+str(threading.active_count()))
 
 lbComm = LoadBalancerCommThread()
 lbComm.start()
 
-# TODO Add in implementation for actual server assets
+cComm = ClientCommThread()
+cComm.start()
 
