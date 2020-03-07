c432f99f7889dfe3d82348bf32b18200f0e168e8
Adam Barton <adamb6102@gmail.com>
2020-03-06 16:25:33 -0800

    Added parser for RTT script.

---
+++ grepper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+grep '1,' $1 > 1.csv
+grep '2,' $1 > 2.csv
+grep '3,' $1 > 3.csv
+grep '4,' $1 > 4.csv
+grep '5,' $1 > 5.csv
+grep '6,' $1 > 6.csv
+grep '7,' $1 > 7.csv
+grep '8,' $1 > 8.csv
+grep '9,' $1 > 9.csv
+grep '10,' $1 > 10.csv
